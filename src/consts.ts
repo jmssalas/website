7359c9af9ae8f35c8c8b75168db4fd29246752fe
jmssalas <hola@jmssalas.com>
2026-09-15 09:11:04 +0200

    Update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'José María Sánchez Salas';
+export const SITE_DESCRIPTION =
+	'Software Engineer & Architect helping engineering teams turn complex systems into reliable software.';
